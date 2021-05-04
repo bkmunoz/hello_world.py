30878e035d7c0736eaf374e98f4e0d1d1ff13093
bkmunoz <82331185+bkmunoz@users.noreply.github.com>
2021-05-03 20:48:35 -0500

    Ninja Bonus 2

--- hello_world.py
+++ hello_world.py
@@ -17,3 +17,22 @@ print(f"I love to eat {fave_food1} and {fave_food2}") # with an f string
 #NINJA BONUS - 1
 name = 42
 print("Hello " + str(name))
+
+#NINJA BONUS - 2
+amount_of_tacos_left = 20
+user_brenna_ate = "16"
+total = amount_of_tacos_left + int(user_brenna_ate)
+print(total)
+
+
+x = "Don't code while hungry"
+print(x.title())
+
+
+bonus = "Hello %s" % "world,"
+py = "I am learning Python %d" % 3
+print(bonus,py)
+
+name = "Brenna"
+age = 28
+print("My name is %s and I'm %d" % (name, age))
